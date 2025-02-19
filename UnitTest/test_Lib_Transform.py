2b7ac66b8ada12069a37d101ceed363f70ad450a
munDayne88 <d.craske.88@gmail.com>
2025-02-19 14:25:10 +0000

    Completed test script

--- UnitTest/test_Lib_Transform.py
+++ UnitTest/test_Lib_Transform.py
@@ -1,11 +1,19 @@
 import unittest
-from UnitTest.Lib_Transform import pd_read_csv
+from Lib_Transform import pd_read_csv
 
 class TestOps(unittest.TestCase):
 
+    def setUp(self):
+        self.rows = pd_read_csv(path = 'C:/Users/Admin/Desktop/GitHub_Stuff/demo/03_Library SystemBook.csv', IdCol='Id', DateCol1='Book checkout', DateCol2='Book Returned',CompareDates=True,BlankCheck='Customer ID')
+        
     def test_func(self):
-        rows = pd_read_csv(path = 'C:/Users/Admin/Desktop/GitHub_Stuff/demo/03_Library SystemBook.csv', IdCol='Id', DateCol1=False, DateCol2=False,CompareDates=False,BlankCheck='Id')
-        self.assertEqual(len(rows.pd_read_csv()), 20, "Odd number of rows")
+        self.assertEqual(len(self.rows), 10, "Wrong number of rows")
+
+    def test_Dur_Pos(self):
+        self.assertTrue((self.rows['Duration'] >= 0).all(), "Duration less than 0")
+
+    def test_Dur_Exc(self):
+        self.assertTrue((self.rows['Duration'] < 365).all(),"Duration more than a year")
         
 if __name__ == '__main__':
     unittest.main()
